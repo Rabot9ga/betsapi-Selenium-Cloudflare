83bb9731b3589cbbf319dc9391262697bf439015
Rabot9ga <freedomboxnikita@inbox.ru>
2024-04-30 09:54:35 +0300

    Trying undetected chrome webdriver (not working)

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
 import time
+import undetected_chromedriver as uc
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
-options = webdriver.ChromeOptions()
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
-driver = webdriver.Chrome(options=options)
-
-driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-    'source': '''
-        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
-        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise;
-        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
-        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Proxy;
-        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Object;
-        delete window.cdc_adoQpoasnfa76pfcZLmcfl_JSON;
-  '''
-})
 
 try:
+    driver = uc.Chrome()
     driver.maximize_window()
     driver.get("http://www.anycoindirect.eu/")
     time.sleep(100)
@@ -32,6 +16,3 @@ finally:
     driver.quit()
 
 
-
-if __name__ == '__main__':
-    print('PyCharm')
